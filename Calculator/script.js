fa03fb55856159fd9d77d6e38cd9c557e9065c17
Godnessy <godnessy@gmail.com>
2022-01-22 21:10:13 +0100

    got to a finished calculator operation

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,19 +1,70 @@
 let result = "";
 let arg1 = "";
-let arg2 = "";
+let arg2 = null;
 let operand = null;
 
-const calc = (num) => {
+function calc(num) {
   if (operand === null) {
-    num === (arg1 += result);
+    arg1 += num;
+    showArg1();
   } else {
-    num.toString((arg2 += result));
+    arg2 = num;
+    showArg2();
+    operator(operand);
   }
+}
 
-  showResult();
+function operator(sign) {
+  operand = sign;
+  showOperand();
+  if (arg2 === null) {
+  } else {
+    switch (sign) {
+      case `+`:
+        a = parseInt(arg1) + parseInt(arg2);
+        result = a.toString();
+        console.log();
+        break;
+      case `-`:
+        a = parseInt(arg1) - parseInt(arg2);
+        result = a.toString();
+
+        break;
+      case `*`:
+        a = parseInt(arg1) * parseInt(arg2);
+        result = a.toString();
+
+        break;
+      case `/`:
+        a = parseInt(arg1) / parseInt(arg2);
+        result = a.toString();
+
+        break;
+    }
+  }
+}
+
+function reset() {
+  result = "";
+  arg1 = "";
+  arg2 = null;
+  operand = null;
+  document.getElementById("result").innerHTML = "";
+  document.getElementById("arg1").innerHTML = arg1;
+  document.getElementById("operand").innerHTML = operand;
+  document.getElementById("arg2").innerHTML = arg2;
+}
+
+//UI functions
+
+const showOperand = () => {
+  document.getElementById("operand").innerHTML = operand;
 };
 
 const showResult = () => {
+  document.getElementById("arg1").innerHTML = "";
+  document.getElementById("arg2").innerHTML = "";
+  document.getElementById("operand").innerHTML = "";
   document.getElementById("result").innerHTML = result;
 };
 const showArg1 = () => {
