ef4fac7651c1adda8b8520dd9174af4da7efed20
Godnessy <godnessy@gmail.com>
2022-01-31 18:48:37 +0100

    refactored HTML to remove JS code from HTML, added code to JS to make it more dry

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,21 +1,43 @@
-"use strict";
-const arg1_ui = document.getElementById("arg1");
-const result_ui = document.getElementById("result");
+'use strict';
+let arg1 = '';
+let arg2 = '';
+let result = '';
 let operand = null;
-const operand_ui = document.getElementById("operand");
-result = "";
-arg1 = "";
+const showUi = (ui, value) => {
+  ui.innerHTML = value;
+};
+const arg1_ui = document.getElementById('arg1');
+const arg2_ui = document.getElementById('arg2');
+const result_ui = document.getElementById('result');
+const operand_ui = document.getElementById('operand');
+
+document.getElementsByName('operationBtn').forEach((a) => {
+  a.addEventListener('click', (a) => {
+    a.target.textContent === 'C' ? reset() : showResult();
+  });
+});
+
+document.getElementsByName('numberBtn').forEach((a) => {
+  a.addEventListener('click', (a) => {
+    calc(a.target.textContent);
+  });
+});
+
+document.getElementsByName('operandBtn').forEach((a) => {
+  a.addEventListener('click', (a) => {
+    operator(a.target.textContent);
+  });
+});
 
 function calc(num) {
   if (operand === null) {
     arg1 += num;
     showUi(arg1_ui, arg1);
-    console.log(`result is ${result} and is ${typeof result}`);
+    console.log(`arg1 is ${arg1} and is ${typeof arg1}`);
   } else {
-    result = num;
-    showUi(result_ui, result);
-    operator(operand);
-    console.log(`arg2 is ${arg2} and is ${typeof result}`);
+    arg2 += num;
+    showUi(arg2_ui, arg2);
+    console.log(`arg2 is ${arg2} and is ${typeof arg2}`);
   }
 }
 
@@ -23,50 +45,50 @@ function operator(sign) {
   operand = sign;
   showUi(operand_ui, operand);
   let a = parseInt(arg1);
-  let b = parseInt(result);
+  let b = parseInt(arg2);
   let c = 0;
-  if (result == "") {
-  } else {
-    switch (sign) {
-      case `+`:
-        c = a + b;
-        break;
-      case `-`:
-        c = a - b;
-        break;
-      case `*`:
-        c = a * b;
-        break;
-      case `/`:
-        c = a / b;
-        break;
-    }
+  switch (sign) {
+    case '+':
+      c = a + b;
+      break;
+    case '-':
+      c = a - b;
+      break;
+    case '*':
+      c = a * b;
+      break;
+    case '/':
+      c = a / b;
+      break;
+    // }
   }
-  console.log(`arg1 (a) is ${a},result  (arg2) is ${b}, c is currently ${c}`);
   result = c.toString();
+  console.log(`arg1 (a) is ${a},arg2 (b) is ${b}, c is currently ${c}`);
+  return result;
 }
 
 // }
 
+// eslint-disable-next-line no-unused-vars
+
 function reset() {
-  result = "";
-  arg1 = "";
-  arg2 = null;
+  result = '';
+  arg1 = '';
+  arg2 = '';
   operand = null;
-  document.getElementById("result").innerHTML = "";
-  document.getElementById("arg1").innerHTML = arg1;
-  document.getElementById("operand").innerHTML = operand;
-  document.getElementById("arg2").innerHTML = arg2;
+  showUi(arg1_ui, arg1);
+  showUi(result_ui, '');
+  showUi(arg2_ui, arg2);
+  showUi(operand_ui, null);
 }
 
 //UI functions
 
-const showResult = () => {
-  showUi(arg1_ui, "");
-  showUi(operand_ui, "");
+// eslint-disable-next-line no-unused-vars
+function showResult() {
+  operator(operand);
+  showUi(arg1_ui, '');
+  showUi(arg2_ui, '');
+  showUi(operand_ui, '');
   showUi(result_ui, result);
-};
-
-const showUi = (ui, value) => {
-  ui.innerHTML = value;
-};
+}
