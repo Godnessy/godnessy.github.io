cc2eb946f64684f35a21b4b944fe6c664ec4af58
Godnessy <84156676+Godnessy@users.noreply.github.com>
2022-02-05 15:10:16 +0100

    Update script.js
    Trying to fix issue with github pages not showing functionality

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,10 +1,10 @@
-/* eslint-disable no-debugger */
 'use strict';
 
 let arg1 = '';
 let arg2 = '';
 let result = '';
 let operand = null;
+const historyBox = [];
 const showUi = (ui, value) => {
   ui.innerHTML = value;
 };
@@ -12,13 +12,16 @@ const arg1_ui = document.getElementById('arg1');
 const arg2_ui = document.getElementById('arg2');
 const result_ui = document.getElementById('result');
 const operand_ui = document.getElementById('operand');
+const history_ui = document.querySelector('.historyBox');
 
+//Number button selector
 document.querySelectorAll('.numberBtn').forEach((a) => {
   a.addEventListener('click', (a) => {
     calc(a.target.textContent);
   });
 });
 
+//Operator button selector
 document.querySelectorAll('.operandBtn').forEach((a) => {
   a.addEventListener('click', (a) => {
     operator(a.target.textContent);
@@ -53,9 +56,6 @@ function operator(sign) {
   const func = operatorMap[sign];
   result = func(int1, int2);
 
-  console.log(
-    `arg1 (a) is ${int1},arg2 (b) is ${int2}, c is currently ${result}, operand is ${operand}`
-  );
   return result;
 }
 
@@ -69,18 +69,20 @@ function reset() {
   showUi(result_ui, '');
   showUi(arg2_ui, arg2);
   showUi(operand_ui, null);
+  recordHistory(result);
+}
+
+function recordHistory() {
+  let history = (document.querySelector('.historyBox').value = result);
+  document.querySelector('.historyBox').innerHTML = history;
+  historyBox.push[result];
 }
 
 //UI functions
-
-// eslint-disable-next-line no-unused-vars
 document.getElementById('equals').onclick = showResult;
 
 function showResult() {
   operator(operand);
-  console.log(
-    `arg1 (a) is ${arg1},arg2 (b) is ${arg2}, c is currently ${result}, operand is ${operand}`
-  );
   operand = null;
   showUi(arg1_ui, '');
   showUi(arg2_ui, '');
@@ -88,4 +90,5 @@ function showResult() {
   showUi(result_ui, result);
   arg1 = result;
   arg2 = '';
+  showUi(history_ui, `Result: ${result}`);
 }
